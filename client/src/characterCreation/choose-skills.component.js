14dabb49685e1bcfe367031a63063b6ce650bef9
Travis DeMint <trav@Orion.local>
2019-03-30 10:34:01 -0500

    basic forms added to each page

--- client/src/characterCreation/choose-skills.component.js
+++ client/src/characterCreation/choose-skills.component.js
@@ -5,7 +5,114 @@ export default class ChooseSkills extends Component {
   render() {
     return (
       <div className='chooseSkills'>
-        <h3>Choose Skills</h3>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="acrobatics"
+                value="acrobatics"
+                />
+        <label className="form-check-label">Acrobatics</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="animalHandling"
+                value="animalHandling"
+                />
+        <label className="form-check-label">Animal Handling</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="arcana"
+                value="arcana"
+                />
+        <label className="form-check-label">Arcana</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="athletics"
+                value="athletics"
+                />
+        <label className="form-check-label">Athletics</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="deception"
+                value="deception"
+                />
+        <label className="form-check-label">Deception</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="history"
+                value="history"
+                />
+        <label className="form-check-label">History</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="insight"
+                value="insight"
+                />
+        <label className="form-check-label">Insight</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="intimidation"
+                value="intimidation"
+                />
+        <label className="form-check-label">Intimidation</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="investigation"
+                value="investigation"
+                />
+        <label className="form-check-label">Investigation</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="medicine"
+                value="medicine"
+                />
+        <label className="form-check-label">Medicine</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="nature"
+                value="nature"
+                />
+        <label className="form-check-label">Nature</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="perception"
+                value="perception"
+                />
+        <label className="form-check-label">Perception</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="performance"
+                value="performance"
+                />
+        <label className="form-check-label">Performance</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="persuation"
+                value="persuation"
+                />
+        <label className="form-check-label">Persuation</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="religion"
+                value="religion"
+                />
+        <label className="form-check-label">Religion</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="sleightOfHand"
+                value="sleightOfHand"
+                />
+        <label className="form-check-label">Sleight of Hand</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="stealth"
+                value="stealth"
+                />
+        <label className="form-check-label">Stealth</label><br/>
+        <input  className="form-check-input"
+                type="checkbox"
+                id="survival"
+                value="survival"
+                />
+        <label className="form-check-label">Survival</label><br/>
       </div>
     );
   }
--- client/src/characterCreation/set-ability-scores.component.js
+++ client/src/characterCreation/set-ability-scores.component.js
@@ -5,7 +5,18 @@ export default class SetAbilityScores extends Component {
   render() {
     return (
       <div className='setAbilityScores'>
-        <h3>Set Ability Scores</h3>
+        <label className="form-check-label">Strength</label>&nbsp;&nbsp;
+        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
+        <label className="form-check-label">Dexterity</label>&nbsp;&nbsp;
+        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
+        <label className="form-check-label">Constitution</label>&nbsp;&nbsp;
+        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
+        <label className="form-check-label">Intelligence</label>&nbsp;&nbsp;
+        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
+        <label className="form-check-label">Wisdom</label>&nbsp;&nbsp;
+        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
+        <label className="form-check-label">Charisma</label>&nbsp;&nbsp;
+        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
       </div>
     );
   }
