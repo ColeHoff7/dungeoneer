1e34ca5133500f552a5d86d36bf8fdd6b2e4b403
Travis DeMint <trav@Orion.local>
2019-03-30 10:48:21 -0500

    fixed numbers for ability scores

--- client/src/characterCreation/set-ability-scores.component.js
+++ client/src/characterCreation/set-ability-scores.component.js
@@ -6,17 +6,17 @@ export default class SetAbilityScores extends Component {
     return (
       <div className='setAbilityScores'>
         <label className="form-check-label">Strength</label>&nbsp;&nbsp;
-        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
+        <input type="number" name="quantity" min="-5" max="15" placeholder="0" /><br/>
         <label className="form-check-label">Dexterity</label>&nbsp;&nbsp;
-        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
+        <input type="number" name="quantity" min="-5" max="15" placeholder="0" /><br/>
         <label className="form-check-label">Constitution</label>&nbsp;&nbsp;
-        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
+        <input type="number" name="quantity" min="-5" max="15" placeholder="0" /><br/>
         <label className="form-check-label">Intelligence</label>&nbsp;&nbsp;
-        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
+        <input type="number" name="quantity" min="-5" max="15" placeholder="0" /><br/>
         <label className="form-check-label">Wisdom</label>&nbsp;&nbsp;
-        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
+        <input type="number" name="quantity" min="-5" max="15" placeholder="0" /><br/>
         <label className="form-check-label">Charisma</label>&nbsp;&nbsp;
-        <input type="number" name="quantity" min="-5" max="15" value="0" /><br/>
+        <input type="number" name="quantity" min="-5" max="15" placeholder="0" /><br/>
       </div>
     );
   }
