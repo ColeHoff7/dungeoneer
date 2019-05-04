5b865ea090c37c03e339cf051d5aed48645bef6d
Cole Hoffbauer <colehoff7@gmail.com>
2019-05-04 17:27:38 -0500

    making initial api endpoints for class, user, character

--- server/api/class.js
+++ server/api/class.js
@@ -1,2 +1,17 @@
 var express = require('express');
 var app = module.exports = express();
+
+// Getting all classes
+app.get('/class', function(req, res){
+    res.json({'message': 'returning all classes'});
+});
+
+// Getting class by id
+app.get('/class/:id([0-9]+)', function(req, res){
+    res.json({message: `getting class by id ${req.param("id")}`});
+});
+
+// Getting class by name
+app.get('/class/:name([A-Za-z]+)', function(req, res){
+    res.json({message: `getting class by name ${req.param("name")}`});
+});
