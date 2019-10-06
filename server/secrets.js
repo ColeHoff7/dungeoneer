1c0e88d4b820d9b228ecc707de9b81a491c2ed7e
Travis DeMint <deminttravis9@gmail.com>
2019-10-06 18:13:46 -0500

    fixed db uri

--- server/secrets.js
+++ server/secrets.js
@@ -1,6 +1,6 @@
 const secrets = {
     //dbUri: "mongodb+srv://dev:dungeoneer@dungeoneer-5jsmy.gcp.mongodb.net/dungeoneer_dev"
-    dbUri: "localhost:27017/dungeoneer_dev"
+    dbUri: "mongodb://localhost:27017/dungeoneer_dev"
 };
 
 export const getSecret = key => secrets[key];
