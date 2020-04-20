6578bc8846478e7f1f0e757f343333dfc750d0ea
Travis DeMint <deminttravis9@gmail.com>
2020-04-20 16:33:29 -0400

    cleaned up skills and ability scores

--- client/src/characterCreation/choose-skills.component.js
+++ client/src/characterCreation/choose-skills.component.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@material-ui/icons/CheckBox';
 
 export default class ChooseSkills extends Component {
 
@@ -13,118 +11,6 @@ export default class ChooseSkills extends Component {
         <SkillCheckboxes />
       </div>
     );
-    // return (
-    //   <div className='chooseSkills'>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="acrobatics"
-    //             value="acrobatics"
-    //             />
-    //     <label className="form-check-label">Acrobatics</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="animalHandling"
-    //             value="animalHandling"
-    //             />
-    //     <label className="form-check-label">Animal Handling</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="arcana"
-    //             value="arcana"
-    //             />
-    //     <label className="form-check-label">Arcana</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="athletics"
-    //             value="athletics"
-    //             />
-    //     <label className="form-check-label">Athletics</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="deception"
-    //             value="deception"
-    //             />
-    //     <label className="form-check-label">Deception</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="history"
-    //             value="history"
-    //             />
-    //     <label className="form-check-label">History</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="insight"
-    //             value="insight"
-    //             />
-    //     <label className="form-check-label">Insight</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="intimidation"
-    //             value="intimidation"
-    //             />
-    //     <label className="form-check-label">Intimidation</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="investigation"
-    //             value="investigation"
-    //             />
-    //     <label className="form-check-label">Investigation</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="medicine"
-    //             value="medicine"
-    //             />
-    //     <label className="form-check-label">Medicine</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="nature"
-    //             value="nature"
-    //             />
-    //     <label className="form-check-label">Nature</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="perception"
-    //             value="perception"
-    //             />
-    //     <label className="form-check-label">Perception</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="performance"
-    //             value="performance"
-    //             />
-    //     <label className="form-check-label">Performance</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="persuation"
-    //             value="persuation"
-    //             />
-    //     <label className="form-check-label">Persuation</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="religion"
-    //             value="religion"
-    //             />
-    //     <label className="form-check-label">Religion</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="sleightOfHand"
-    //             value="sleightOfHand"
-    //             />
-    //     <label className="form-check-label">Sleight of Hand</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="stealth"
-    //             value="stealth"
-    //             />
-    //     <label className="form-check-label">Stealth</label><br/>
-    //     <input  className="form-check-input"
-    //             type="checkbox"
-    //             id="survival"
-    //             value="survival"
-    //             />
-    //     <label className="form-check-label">Survival</label><br/>
-    //   </div>
-    // );
   }
 
 }
--- client/src/characterCreation/set-ability-scores.component.js
+++ client/src/characterCreation/set-ability-scores.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
+import FormGroup from '@material-ui/core/FormGroup';
 
 import './character-creation.css';
 
@@ -8,14 +9,61 @@ export default class SetAbilityScores extends Component {
   render() {
     return (
       <div className='setAbilityScores'>
-        <TextField className="ability-score-input" id="strengthScore" label="Strength" variant="outlined" defaultValue="0" /><br/><br/>
-        <TextField className="ability-score-input" id="dexterityScore" label="Dexterity" variant="outlined" defaultValue="0" /><br/><br/>
-        <TextField className="ability-score-input" id="constitutionScore" label="Constitution" variant="outlined" defaultValue="0" /><br/><br/>
-        <TextField className="ability-score-input" id="intelligenceScore" label="Intelligence" variant="outlined" defaultValue="0" /><br/><br/>
-        <TextField className="ability-score-input" id="wisdomScore" label="Wisdom" variant="outlined" defaultValue="0" /><br/><br/>
-        <TextField className="ability-score-input" id="charismaScore" label="Charisma" variant="outlined" defaultValue="0" />
+        <AbilityScoreFields />
       </div>
     );
   }
+}
 
+function AbilityScoreFields() {
+  return (
+    <FormGroup row>
+      <FormGroup>
+        <TextField
+          className="ability-score-input"
+          id="strengthScore"
+          label="Strength"
+          variant="outlined"
+          defaultValue="0"
+        />
+        <TextField
+          className="ability-score-input"
+          id="dexterityScore"
+          label="Dexterity"
+          variant="outlined"
+          defaultValue="0"
+        />
+        <TextField
+          className="ability-score-input"
+          id="constitutionScore"
+          label="Constitution"
+          variant="outlined"
+          defaultValue="0"
+        />
+      </FormGroup>
+      <FormGroup>
+      <TextField
+        className="ability-score-input"
+        id="intelligenceScore"
+        label="Intelligence"
+        variant="outlined"
+        defaultValue="0"
+      />
+      <TextField
+        className="ability-score-input"
+        id="wisdomScore"
+        label="Wisdom"
+        variant="outlined"
+        defaultValue="0"
+      />
+      <TextField
+        className="ability-score-input"
+        id="charismaScore"
+        label="Charisma"
+        variant="outlined"
+        defaultValue="0"
+      />
+      </FormGroup>
+    </FormGroup>
+  )
 }
