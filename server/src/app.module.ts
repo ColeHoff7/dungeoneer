3b58bcdcc00a8dfad26f9f33b327ad453b2bc6b4
Cole Hoffbauer <colehoff7@gmail.com>
2020-05-30 13:08:34 -0500

    adding character class to app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RaceModule } from './race/race.module';
+import { CharacterClassModule } from './character_class/character-class.module';
 import { getSecret } from './secrets';
 
 @Module({
-  imports: [MongooseModule.forRoot(getSecret('dbUri'), {useNewUrlParser: true}), RaceModule],
+  imports: [MongooseModule.forRoot(getSecret('dbUri'), {useNewUrlParser: true}), RaceModule, CharacterClassModule],
 })
 export class AppModule {}
