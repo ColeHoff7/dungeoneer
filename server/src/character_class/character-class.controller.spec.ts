22d1ef79290efec5e1ee609a0ef4835cd092d18c
Cole Hoffbauer <colehoff7@gmail.com>
2020-05-30 13:07:02 -0500

    migrating character class to nest

---
+++ server/src/character_class/character-class.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CharacterClassController } from './character-class.controller';
+import { CharacterClassService } from './character-class.service';
+
+describe('CharacterClassController', () => {
+  let characterClassController: CharacterClassController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [CharacterClassController],
+      providers: [CharacterClassService],
+    }).compile();
+
+    characterClassController = app.get<CharacterClassController>(CharacterClassController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello Class!"', () => {
+      expect(characterClassController.get()).toBe('Hello CharacterClass!');
+    });
+  });
+});
