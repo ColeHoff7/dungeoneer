cf820c989077aa2ff1d380440b3c5755ada3ebd9
Travis DeMint <deminttravis9@gmail.com>
2020-06-01 21:51:09 -0400

    Delete secrets.ts

--- server/src/secrets.ts
+++
@@ -1,6 +0,0 @@
-const secrets = {
-    //dbUri: "mongodb+srv://dev:dungeoneer@dungeoneer-5jsmy.gcp.mongodb.net/dungeoneer_dev"
-    dbUri: "mongodb://localhost:27017/dungeoneer_dev"
-};
-
-export const getSecret = key => secrets[key];
